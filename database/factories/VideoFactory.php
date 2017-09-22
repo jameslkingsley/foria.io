c139a17a866d2c3d1cc5e290d7513f73a150dfc3
James Kingsley <jameslkingsley@outlook.com>
2017-09-22 18:10:24 +0100

    Work in progress

--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -5,11 +5,10 @@ use App\Models\Video;
 use Faker\Generator as Faker;
 
 $factory->define(Video::class, function (Faker $faker) {
-    $key = str_random(10);
     $requireSubscription = $faker->boolean;
 
     return [
-        'key' => $key,
+        'key' => '6c8a07b274b682c1261828f904e3436c',
         'name' => $faker->city,
         'user_id' => User::where('is_model', true)->inRandomOrder()->first()->id,
         'token_price' => $requireSubscription ? null : $faker->numberBetween(0, 10000),
